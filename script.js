9ce5d748eb240f7af103cd92b318a05e8d3d0b8b
Tayyab Kharl <tayyabmahr@gmail.com>
2020-05-15 15:50:20 +0500

    version alpha
    This is where real magic happens.

---
+++ script.js
@@ -0,0 +1,370 @@
+function GPA(){
+  var qp1 = 3
+  var qp2 = 3.5
+  var qp3 = 4
+  var qp4 = 4.5
+  var qp5 = 5
+  var qp6 = 5.5
+  var qp7 = 6
+  var qp8 = 6.33
+  var qp9 = 6.67
+  var qp10 = 7
+  var qp11 = 7.33
+  var qp12 = 7.67
+  var qp13 = 8
+  var qp14 = 8.33
+  var qp15 = 8.67
+  var qp16 = 9
+  var qp17 = 9.33
+  var qp18 = 9.67
+  var qp19 = 10
+  var qp20 = 10.33
+  var qp21 = 10.67
+  var qp22 = 11
+  var qp23 = 11.33
+  var qp24 = 11.67
+  var qp25 = 12
+  var qp26 = 0
+
+  var a = document.getElementById("subject1").value;
+  var b = document.getElementById("subject2").value;
+  var c = document.getElementById("subject3").value;
+  var d = document.getElementById("subject4").value;
+  var e = document.getElementById("subject5").value;
+  var f = document.getElementById("subject6").value;
+  var g = document.getElementById("crehrs").value;
+
+  if (a == 24) {
+      var a1 = qp1;
+  }else if (a == 25) {
+    var a1 = qp2;
+  }else if (a == 26) {
+    var a1 = qp3;
+  }else if (a == 27) {
+    var a1 = qp4;
+  }else if (a == 28) {
+    var a1 = qp5;
+  }else if (a == 29) {
+    var a1 = qp6;
+  }else if (a == 30) {
+    var a1 = qp7;
+  }else if (a == 31) {
+    var a1 = qp8;
+  }else if (a == 32) {
+    var a1 = qp9;
+  }else if (a == 33) {
+    var a1 = qp10;
+  }else if (a == 34) {
+    var a1 = qp11;
+  }else if (a == 35) {
+    var a1 = qp12;
+  }else if (a == 36) {
+    var a1 = qp13;
+  }else if (a == 37) {
+    var a1 = qp14;
+  }else if (a == 38) {
+    var a1 = qp15;
+  }else if (a == 39) {
+    var a1 = qp16;
+  }else if (a == 40) {
+    var a1 = qp17;
+  }else if (a == 41) {
+    var a1 = qp18;
+  }else if (a == 42) {
+    var a1 = qp19;
+  }else if (a == 43) {
+    var a1 = qp20;
+  }else if (a == 44) {
+    var a1 = qp21;
+  }else if (a == 45) {
+    var a1 = qp22;
+  }else if (a == 46) {
+    var a1 = qp23;
+  }else if (a == 47) {
+    var a1 = qp24;
+  }else if ((a >= 48) && (a <= 60)) {
+    var a1 = qp25;
+  }else {
+    var a1 = qp26;
+  }
+
+
+  if (b == 24) {
+      var b1 = qp1;
+  }else if (b == 25) {
+    var b1 = qp2;
+  }else if (b == 26) {
+    var b1 = qp3;
+  }else if (b == 27) {
+    var b1 = qp4;
+  }else if (b == 28) {
+    var b1 = qp5;
+  }else if (b == 29) {
+    var b1 = qp6;
+  }else if (b == 30) {
+    var b1 = qp7;
+  }else if (b == 31) {
+    var b1 = qp8;
+  }else if (b == 32) {
+    var b1 = qp9;
+  }else if (b == 33) {
+    var b1 = qp10;
+  }else if (b == 34) {
+    var b1 = qp11;
+  }else if (b == 35) {
+    var b1 = qp12;
+  }else if (b == 36) {
+    var b1 = qp13;
+  }else if (b == 37) {
+    var b1 = qp14;
+  }else if (b == 38) {
+    var b1 = qp15;
+  }else if (b == 39) {
+    var b1 = qp16;
+  }else if (b == 40) {
+    var b1 = qp17;
+  }else if (b == 41) {
+    var b1 = qp18;
+  }else if (b == 42) {
+    var b1 = qp19;
+  }else if (b == 43) {
+    var b1 = qp20;
+  }else if (b == 44) {
+    var b1 = qp21;
+  }else if (b == 45) {
+    var b1 = qp22;
+  }else if (b == 46) {
+    var b1 = qp23;
+  }else if (b == 47) {
+    var b1 = qp24;
+  }else if ((b >= 48) && (b <= 60)) {
+    var b1 = qp25;
+  }else {
+    var b1 = qp26;
+  }
+
+
+
+  if (c == 24) {
+      var c1 = qp1;
+  }else if (c == 25) {
+    var c1 = qp2;
+  }else if (c == 26) {
+    var c1 = qp3;
+  }else if (c == 27) {
+    var c1 = qp4;
+  }else if (c == 28) {
+    var c1 = qp5;
+  }else if (c == 29) {
+    var c1 = qp6;
+  }else if (c == 30) {
+    var c1 = qp7;
+  }else if (c == 31) {
+    var c1 = qp8;
+  }else if (c == 32) {
+    var c1 = qp9;
+  }else if (c == 33) {
+    var c1 = qp10;
+  }else if (c == 34) {
+    var c1 = qp11;
+  }else if (c == 35) {
+    var c1 = qp12;
+  }else if (c == 36) {
+    var c1 = qp13;
+  }else if (c == 37) {
+    var c1 = qp14;
+  }else if (c == 38) {
+    var c1 = qp15;
+  }else if (c == 39) {
+    var c1 = qp16;
+  }else if (c == 40) {
+    var c1 = qp17;
+  }else if (c == 41) {
+    var c1 = qp18;
+  }else if (c == 42) {
+    var c1 = qp19;
+  }else if (c == 43) {
+    var c1 = qp20;
+  }else if (c == 44) {
+    var c1 = qp21;
+  }else if (c == 45) {
+    var c1 = qp22;
+  }else if (c == 46) {
+    var c1 = qp23;
+  }else if (c == 47) {
+    var c1 = qp24;
+  }else if ((c >= 48) && (c <= 60)) {
+    var c1 = qp25;
+  }else {
+    var c1 = qp26;
+  }
+
+  if (d == 24) {
+      var d1 = qp1;
+  }else if (d == 25) {
+    var d1 = qp2;
+  }else if (d == 26) {
+    var d1 = qp3;
+  }else if (d == 27) {
+    var d1 = qp4;
+  }else if (d == 28) {
+    var d1 = qp5;
+  }else if (d == 29) {
+    var d1 = qp6;
+  }else if (d == 30) {
+    var d1 = qp7;
+  }else if (d == 31) {
+    var d1 = qp8;
+  }else if (d == 32) {
+    var d1 = qp9;
+  }else if (d == 33) {
+    var d1 = qp10;
+  }else if (d == 34) {
+    var d1 = qp11;
+  }else if (d == 35) {
+    var d1 = qp12;
+  }else if (d == 36) {
+    var d1 = qp13;
+  }else if (d == 37) {
+    var d1 = qp14;
+  }else if (d == 38) {
+    var d1 = qp15;
+  }else if (d == 39) {
+    var d1 = qp16;
+  }else if (d == 40) {
+    var d1 = qp17;
+  }else if (d == 41) {
+    var d1 = qp18;
+  }else if (d == 42) {
+    var d1 = qp19;
+  }else if (d == 43) {
+    var d1 = qp20;
+  }else if (d == 44) {
+    var d1 = qp21;
+  }else if (d == 45) {
+    var d1 = qp22;
+  }else if (d == 46) {
+    var d1 = qp23;
+  }else if (d == 47) {
+    var d1 = qp24;
+  }else if ((d >= 48) && (d <= 60)) {
+    var d1 = qp25;
+  }else {
+    var d1 = qp26;
+  }
+
+  if (e == 24) {
+      var e1 = qp1;
+  }else if (e == 25) {
+    var e1 = qp2;
+  }else if (e == 26) {
+    var e1 = qp3;
+  }else if (e == 27) {
+    var e1 = qp4;
+  }else if (e == 28) {
+    var e1 = qp5;
+  }else if (e == 29) {
+    var e1 = qp6;
+  }else if (e == 30) {
+    var e1 = qp7;
+  }else if (e == 31) {
+    var e1 = qp8;
+  }else if (e == 32) {
+    var e1 = qp9;
+  }else if (e == 33) {
+    var e1 = qp10;
+  }else if (e == 34) {
+    var e1 = qp11;
+  }else if (e == 35) {
+    var e1 = qp12;
+  }else if (e == 36) {
+    var e1 = qp13;
+  }else if (e == 37) {
+    var e1 = qp14;
+  }else if (e == 38) {
+    var e1 = qp15;
+  }else if (e == 39) {
+    var e1 = qp16;
+  }else if (e == 40) {
+    var e1 = qp17;
+  }else if (e == 41) {
+    var e1 = qp18;
+  }else if (e == 42) {
+    var e1 = qp19;
+  }else if (e == 43) {
+    var e1 = qp20;
+  }else if (e == 44) {
+    var e1 = qp21;
+  }else if (e == 45) {
+    var e1 = qp22;
+  }else if (e == 46) {
+    var e1 = qp23;
+  }else if (e == 47) {
+    var e1 = qp24;
+  }else if ((e >= 48) && (e <= 60)) {
+    var e1 = qp25;
+  }else {
+    var e1 = qp26;
+  }
+
+
+  if (f == 24) {
+      var f1 = qp1;
+  }else if (f == 25) {
+    var f1 = qp2;
+  }else if (f == 26) {
+    var f1 = qp3;
+  }else if (f == 27) {
+    var f1 = qp4;
+  }else if (f == 28) {
+    var f1 = qp5;
+  }else if (f == 29) {
+    var f1 = qp6;
+  }else if (f == 30) {
+    var f1 = qp7;
+  }else if (f == 31) {
+    var f1 = qp8;
+  }else if (f == 32) {
+    var f1 = qp9;
+  }else if (f == 33) {
+    var f1 = qp10;
+  }else if (f == 34) {
+    var f1 = qp11;
+  }else if (f == 35) {
+    var f1 = qp12;
+  }else if (f == 36) {
+    var f1 = qp13;
+  }else if (f == 37) {
+    var f1 = qp14;
+  }else if (f == 38) {
+    var f1 = qp15;
+  }else if (f == 39) {
+    var f1 = qp16;
+  }else if (f == 40) {
+    var f1 = qp17;
+  }else if (f == 41) {
+    var f1 = qp18;
+  }else if (f == 42) {
+    var f1 = qp19;
+  }else if (f == 43) {
+    var f1 = qp20;
+  }else if (f == 44) {
+    var f1 = qp21;
+  }else if (f == 45) {
+    var f1 = qp22;
+  }else if (f == 46) {
+    var f1 = qp23;
+  }else if (f == 47) {
+    var f1 = qp24;
+  }else if ((f >= 48) && (f <= 60)) {
+    var f1 = qp25;
+  }else {
+    var f1 = qp26;
+  }
+
+  var sum = parseFloat(a1) + parseFloat(b1) + parseFloat(c1) + parseFloat(d1) + parseFloat(e1) + parseFloat(f1);
+  var raw = (sum/g);
+  var result = raw.toFixed(2)
+  document.getElementById("r").textContent = "Your GPA is:"+ result;
+  return false;
+}
