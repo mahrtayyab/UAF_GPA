b544a15258481b4f0241a9bde38696ee6149450b
Tayyab Kharl <tayyabmahr@gmail.com>
2020-06-24 16:09:32 +0500

    Update 3.0
    Added capability to calculate the GPA of courses having 80 marks.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function GPA60(){
+  //variables for courses having 20 marks
   var qp21 = 1
   var qp22 = 1.5
   var qp23 = 2
@@ -9,6 +10,7 @@ function GPA60(){
   var qp28 = 3.67
   var qp29 = 4
   var qp20 = 0
+  //variables for courses having 60 marks
   var qp61 = 3
   var qp62 = 3.5
   var qp63 = 4
@@ -34,6 +36,7 @@ function GPA60(){
   var qp83 = 11.33
   var qp84 = 11.67
   var qp85 = 12
+  //variables for courses having 40 marks
   var qp41 = 2
   var qp42 = 2.5
   var qp43 = 3
@@ -51,6 +54,65 @@ function GPA60(){
   var qp55 = 7.33
   var qp56 = 7.67
   var qp57 = 8
+  var qp86 = 4
+  //variable for courses having 80 marks
+  var qp87 = 4.5
+  var qp88 = 5
+  var qp89 = 5.5
+  var qp90 = 6
+  var qp91 = 6.5
+  var qp91 = 7
+  var qp92 = 7.5
+  var qp93 = 8
+  var qp94 = 8.33
+  var qp95 = 8.67
+  var qp96 = 9
+  var qp97 = 9.33
+  var qp98 = 9.67
+  var qp99 = 10
+  var qp100 = 10.33
+  var qp101 = 10.67
+  var qp102 = 11
+  var qp103 = 11.33
+  var qp104 = 11.67
+  var qp105 = 12
+  var qp106 = 12.33
+  var qp107 = 12.67
+  var qp108 = 13
+  var qp109 = 13.33
+  var qp110 = 13.67
+  var qp111 = 14
+  var qp112 = 14.33
+  var qp113 = 14.67
+  var qp114 = 15
+  var qp116 = 15.33
+  var qp117 = 15.67
+  var qp118 = 16
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   var a = document.getElementById("subject1o").value;
   var b = document.getElementById("subject2o").value;
@@ -169,9 +231,75 @@ function GPA60(){
     var a1 = qp56;
   }else if ((a >= 32) && (a <= 40) && (h == 40)){
     var a1 = qp57;
+  }else if ((a == 32) && (h == 80)) {
+    var a1 = qp86;
+  }else if ((a == 33) && (h == 80)) {
+    var a1 = qp87;
+  }else if ((a == 34) && (h == 80)) {
+    var a1 = qp88;
+  }else if ((a == 35) && (h == 80)) {
+    var a1 = qp89;
+  }else if ((a == 36) && (h == 80)) {
+    var a1 = qp90;
+  }else if ((a == 37) && (h == 80)) {
+    var a1 = qp91;
+  }else if ((a == 38) && (h == 80)) {
+    var a1 = qp92;
+  }else if ((a == 39) && (h == 80)) {
+    var a1 = qp93;
+  }else if ((a == 40) && (h == 80)) {
+    var a1 = qp94;
+  }else if ((a == 41) && (h == 80)) {
+    var a1 = qp95;
+  }else if ((a == 42) && (h == 80)) {
+    var a1 = qp96;
+  }else if ((a == 43) && (h == 80)) {
+    var a1 = qp97;
+  }else if ((a == 44) && (h == 80)) {
+    var a1 = qp98;
+  }else if ((a == 45) && (h == 80)) {
+    var a1 = qp99;
+  }else if ((a == 46) && (h == 80)) {
+    var a1 = qp100;
+  }else if ((a == 47) && (h == 80)) {
+    var a1 = qp101;
+  }else if ((a == 48) && (h == 80)) {
+    var a1 = qp102;
+  }else if ((a == 49) && (h == 80)) {
+    var a1 = qp103;
+  }else if ((a == 50) && (h == 80)) {
+    var a1 = qp104;
+  }else if ((a == 51) && (h == 80)) {
+    var a1 = qp105;
+  }else if ((a == 52) && (h == 80)) {
+    var a1 = qp106;
+  }else if ((a == 53) && (h == 80)) {
+    var a1 = qp107;
+  }else if ((a == 54) && (h == 80)) {
+    var a1 = qp108;
+  }else if ((a == 55) && (h == 80)) {
+    var a1 = qp109;
+  }else if ((a == 56) && (h == 80)) {
+    var a1 = qp110;
+  }else if ((a == 57) && (h == 80)) {
+    var a1 = qp111;
+  }else if ((a == 58) && (h == 80)) {
+    var a1 = qp112;
+  }else if ((a == 59) && (h == 80)) {
+    var a1 = qp113;
+  }else if ((a == 60) && (h == 80)) {
+    var a1 = qp114;
+  }else if ((a == 61) && (h == 80)) {
+    var a1 = qp115;
+  }else if ((a == 62) && (h == 80)) {
+    var a1 = qp116;
+  }else if ((a == 63) && (h == 80)) {
+    var a1 = qp117;
+  }else if ((a >= 64) && (a <= 80) && (h == 80)) {
+    var a1 = qp118;
   }else {
     var a1 = qp20;
-}
+  }
 
 
   if ((b == 24) && (i == 60))  {
@@ -276,6 +404,72 @@ function GPA60(){
     var b1 = qp56;
   }else if ((b >= 32) && (b <= 40) && (i == 40)){
     var b1 = qp57;
+  }else if ((b == 32) && (i == 80)) {
+    var b1 = qp86;
+  }else if ((b == 33) && (i == 80)) {
+    var b1 = qp87;
+  }else if ((b == 34) && (i == 80)) {
+    var b1 = qp88;
+  }else if ((b == 35) && (i == 80)) {
+    var b1 = qp89;
+  }else if ((b == 36) && (i == 80)) {
+    var b1 = qp90;
+  }else if ((b == 37) && (i == 80)) {
+    var b1 = qp91;
+  }else if ((b == 38) && (i == 80)) {
+    var b1 = qp92;
+  }else if ((b == 39) && (i == 80)) {
+    var b1 = qp93;
+  }else if ((b == 40) && (i == 80)) {
+    var b1 = qp94;
+  }else if ((b == 41) && (i == 80)) {
+    var b1 = qp95;
+  }else if ((b == 42) && (i == 80)) {
+    var b1 = qp96;
+  }else if ((b == 43) && (i == 80)) {
+    var b1 = qp97;
+  }else if ((b == 44) && (i == 80)) {
+    var b1 = qp98;
+  }else if ((b == 45) && (i == 80)) {
+    var b1 = qp99;
+  }else if ((b == 46) && (i == 80)) {
+    var b1 = qp100;
+  }else if ((b == 47) && (i == 80)) {
+    var b1 = qp101;
+  }else if ((b == 48) && (i == 80)) {
+    var b1 = qp102;
+  }else if ((b == 49) && (i == 80)) {
+    var b1 = qp103;
+  }else if ((b == 50) && (i == 80)) {
+    var b1 = qp104;
+  }else if ((b == 51) && (i == 80)) {
+    var b1 = qp105;
+  }else if ((b == 52) && (i == 80)) {
+    var b1 = qp106;
+  }else if ((b == 53) && (i == 80)) {
+    var b1 = qp107;
+  }else if ((b == 54) && (i == 80)) {
+    var b1 = qp108;
+  }else if ((b == 55) && (i == 80)) {
+    var b1 = qp109;
+  }else if ((b == 56) && (i == 80)) {
+    var b1 = qp110;
+  }else if ((b == 57) && (i == 80)) {
+    var b1 = qp111;
+  }else if ((b == 58) && (i == 80)) {
+    var b1 = qp112;
+  }else if ((b == 59) && (i == 80)) {
+    var b1 = qp113;
+  }else if ((b == 60) && (i == 80)) {
+    var b1 = qp114;
+  }else if ((b == 61) && (i == 80)) {
+    var b1 = qp115;
+  }else if ((b == 62) && (i == 80)) {
+    var b1 = qp116;
+  }else if ((b == 63) && (i == 80)) {
+    var b1 = qp117;
+  }else if ((b >= 64) && (b <= 80) && (i == 80)) {
+    var b1 = qp118;
   }else {
     var b1 = qp20;
   }
@@ -384,6 +578,72 @@ function GPA60(){
     var c1 = qp56;
   }else if ((c >= 32) && (c <= 40) && (j == 40)){
     var c1 = qp57;
+  }else if ((c == 32) && (j == 80)) {
+    var c1 = qp86;
+  }else if ((c == 33) && (j == 80)) {
+    var c1 = qp87;
+  }else if ((c == 34) && (j == 80)) {
+    var c1 = qp88;
+  }else if ((c == 35) && (j == 80)) {
+    var c1 = qp89;
+  }else if ((c == 36) && (j == 80)) {
+    var c1 = qp90;
+  }else if ((c == 37) && (j == 80)) {
+    var c1 = qp91;
+  }else if ((c == 38) && (j == 80)) {
+    var c1 = qp92;
+  }else if ((c == 39) && (j == 80)) {
+    var c1 = qp93;
+  }else if ((c == 40) && (j == 80)) {
+    var c1 = qp94;
+  }else if ((c == 41) && (j == 80)) {
+    var c1 = qp95;
+  }else if ((c == 42) && (j == 80)) {
+    var c1 = qp96;
+  }else if ((c == 43) && (j == 80)) {
+    var c1 = qp97;
+  }else if ((c == 44) && (j == 80)) {
+    var c1 = qp98;
+  }else if ((c == 45) && (j == 80)) {
+    var c1 = qp99;
+  }else if ((c == 46) && (j == 80)) {
+    var c1 = qp100;
+  }else if ((c == 47) && (j == 80)) {
+    var c1 = qp101;
+  }else if ((c == 48) && (j == 80)) {
+    var c1 = qp102;
+  }else if ((c == 49) && (j == 80)) {
+    var c1 = qp103;
+  }else if ((c == 50) && (j == 80)) {
+    var c1 = qp104;
+  }else if ((c == 51) && (j == 80)) {
+    var c1 = qp105;
+  }else if ((c == 52) && (j == 80)) {
+    var c1 = qp106;
+  }else if ((c == 53) && (j == 80)) {
+    var c1 = qp107;
+  }else if ((c == 54) && (j == 80)) {
+    var c1 = qp108;
+  }else if ((c == 55) && (j == 80)) {
+    var c1 = qp109;
+  }else if ((c == 56) && (j == 80)) {
+    var c1 = qp110;
+  }else if ((c == 57) && (j == 80)) {
+    var c1 = qp111;
+  }else if ((c == 58) && (j == 80)) {
+    var c1 = qp112;
+  }else if ((c == 59) && (j == 80)) {
+    var c1 = qp113;
+  }else if ((c == 60) && (j == 80)) {
+    var c1 = qp114;
+  }else if ((c == 61) && (j == 80)) {
+    var c1 = qp115;
+  }else if ((c == 62) && (j == 80)) {
+    var c1 = qp116;
+  }else if ((c == 63) && (j == 80)) {
+    var c1 = qp117;
+  }else if ((c >= 64) && (c <= 80) && (j == 80)) {
+    var c1 = qp118;
   }else {
     var c1 = qp20;
   }
@@ -491,6 +751,72 @@ function GPA60(){
     var d1 = qp56;
   }else if ((d >= 32) && (d <= 40) && (k == 40)){
     var d1 = qp57;
+  }else if ((d == 32) && (k == 80)) {
+    var d1 = qp86;
+  }else if ((d == 33) && (k == 80)) {
+    var d1 = qp87;
+  }else if ((d == 34) && (k == 80)) {
+    var d1 = qp88;
+  }else if ((d == 35) && (k == 80)) {
+    var d1 = qp89;
+  }else if ((d == 36) && (k == 80)) {
+    var d1 = qp90;
+  }else if ((d == 37) && (k == 80)) {
+    var d1 = qp91;
+  }else if ((d == 38) && (k == 80)) {
+    var d1 = qp92;
+  }else if ((d == 39) && (k == 80)) {
+    var d1 = qp93;
+  }else if ((d == 40) && (k == 80)) {
+    var d1 = qp94;
+  }else if ((d == 41) && (k == 80)) {
+    var d1 = qp95;
+  }else if ((d == 42) && (k == 80)) {
+    var d1 = qp96;
+  }else if ((d == 43) && (k == 80)) {
+    var d1 = qp97;
+  }else if ((d == 44) && (k == 80)) {
+    var d1 = qp98;
+  }else if ((d == 45) && (k == 80)) {
+    var d1 = qp99;
+  }else if ((d == 46) && (k == 80)) {
+    var d1 = qp100;
+  }else if ((d == 47) && (k == 80)) {
+    var d1 = qp101;
+  }else if ((d == 48) && (k == 80)) {
+    var d1 = qp102;
+  }else if ((d == 49) && (k == 80)) {
+    var d1 = qp103;
+  }else if ((d == 50) && (k == 80)) {
+    var d1 = qp104;
+  }else if ((d == 51) && (k == 80)) {
+    var d1 = qp105;
+  }else if ((d == 52) && (k == 80)) {
+    var d1 = qp106;
+  }else if ((d == 53) && (k == 80)) {
+    var d1 = qp107;
+  }else if ((d == 54) && (k == 80)) {
+    var d1 = qp108;
+  }else if ((d == 55) && (k == 80)) {
+    var d1 = qp109;
+  }else if ((d == 56) && (k == 80)) {
+    var d1 = qp110;
+  }else if ((d == 57) && (k == 80)) {
+    var d1 = qp111;
+  }else if ((d == 58) && (k == 80)) {
+    var d1 = qp112;
+  }else if ((d == 59) && (k == 80)) {
+    var d1 = qp113;
+  }else if ((d == 60) && (k == 80)) {
+    var d1 = qp114;
+  }else if ((d == 61) && (k == 80)) {
+    var d1 = qp115;
+  }else if ((d == 62) && (k == 80)) {
+    var d1 = qp116;
+  }else if ((d == 63) && (k == 80)) {
+    var d1 = qp117;
+  }else if ((d >= 64) && (d <= 80) && (k == 80)) {
+    var d1 = qp118;
   }else {
     var d1 = qp20;
   }
@@ -599,6 +925,72 @@ function GPA60(){
     var e1 = qp56;
   }else if ((e >= 32) && (e <= 40) && (l == 40)){
     var e1 = qp57;
+  }else if ((e == 32) && (l == 80)) {
+    var e1 = qp86;
+  }else if ((e == 33) && (l == 80)) {
+    var e1 = qp87;
+  }else if ((e == 34) && (l == 80)) {
+    var e1 = qp88;
+  }else if ((e == 35) && (l == 80)) {
+    var e1 = qp89;
+  }else if ((e == 36) && (l == 80)) {
+    var e1 = qp90;
+  }else if ((e == 37) && (l == 80)) {
+    var e1 = qp91;
+  }else if ((e == 38) && (l == 80)) {
+    var e1 = qp92;
+  }else if ((e == 39) && (l == 80)) {
+    var e1 = qp93;
+  }else if ((e == 40) && (l == 80)) {
+    var e1 = qp94;
+  }else if ((e == 41) && (l == 80)) {
+    var e1 = qp95;
+  }else if ((e == 42) && (l == 80)) {
+    var e1 = qp96;
+  }else if ((e == 43) && (l == 80)) {
+    var e1 = qp97;
+  }else if ((e == 44) && (l == 80)) {
+    var e1 = qp98;
+  }else if ((e == 45) && (l == 80)) {
+    var e1 = qp99;
+  }else if ((e == 46) && (l == 80)) {
+    var e1 = qp100;
+  }else if ((e == 47) && (l == 80)) {
+    var e1 = qp101;
+  }else if ((e == 48) && (l == 80)) {
+    var e1 = qp102;
+  }else if ((e == 49) && (l == 80)) {
+    var e1 = qp103;
+  }else if ((e == 50) && (l == 80)) {
+    var e1 = qp104;
+  }else if ((e == 51) && (l == 80)) {
+    var e1 = qp105;
+  }else if ((e == 52) && (l == 80)) {
+    var e1 = qp106;
+  }else if ((e == 53) && (l == 80)) {
+    var e1 = qp107;
+  }else if ((e == 54) && (l == 80)) {
+    var e1 = qp108;
+  }else if ((e == 55) && (l == 80)) {
+    var e1 = qp109;
+  }else if ((e == 56) && (l == 80)) {
+    var e1 = qp110;
+  }else if ((e == 57) && (l == 80)) {
+    var e1 = qp111;
+  }else if ((e == 58) && (l == 80)) {
+    var e1 = qp112;
+  }else if ((e == 59) && (l == 80)) {
+    var e1 = qp113;
+  }else if ((e == 60) && (l == 80)) {
+    var e1 = qp114;
+  }else if ((e == 61) && (l == 80)) {
+    var e1 = qp115;
+  }else if ((e == 62) && (l == 80)) {
+    var e1 = qp116;
+  }else if ((e == 63) && (l == 80)) {
+    var e1 = qp117;
+  }else if ((e >= 64) && (e <= 80) && (l == 80)) {
+    var e1 = qp118;
   }else {
     var e1 = qp20;
   }
@@ -708,14 +1100,82 @@ function GPA60(){
     var f1 = qp56;
   }else if ((f >= 32) && (f <= 40) && (m == 40)){
     var f1 = qp57;
-  }else {
+  }else if ((f == 32) && (m == 80)) {
+    var f1 = qp86;
+  }else if ((f == 33) && (m == 80)) {
+    var f1 = qp87;
+  }else if ((f == 34) && (m == 80)) {
+    var f1 = qp88;
+  }else if ((f == 35) && (m == 80)) {
+    var f1 = qp89;
+  }else if ((f == 36) && (m == 80)) {
+    var f1 = qp90;
+  }else if ((f == 37) && (m == 80)) {
+    var f1 = qp91;
+  }else if ((f == 38) && (m == 80)) {
+    var f1 = qp92;
+  }else if ((f == 39) && (m == 80)) {
+    var f1 = qp93;
+  }else if ((f == 40) && (m == 80)) {
+    var f1 = qp94;
+  }else if ((f == 41) && (m == 80)) {
+    var f1 = qp95;
+  }else if ((f == 42) && (m == 80)) {
+    var f1 = qp96;
+  }else if ((f == 43) && (m == 80)) {
+    var f1 = qp97;
+  }else if ((f == 44) && (m == 80)) {
+    var f1 = qp98;
+  }else if ((f == 45) && (m == 80)) {
+    var f1 = qp99;
+  }else if ((f == 46) && (m == 80)) {
+    var f1 = qp100;
+  }else if ((f == 47) && (m == 80)) {
+    var f1 = qp101;
+  }else if ((f == 48) && (m == 80)) {
+    var f1 = qp102;
+  }else if ((f == 49) && (m == 80)) {
+    var f1 = qp103;
+  }else if ((f == 50) && (m == 80)) {
+    var f1 = qp104;
+  }else if ((f == 51) && (m == 80)) {
+    var f1 = qp105;
+  }else if ((f == 52) && (m == 80)) {
+    var f1 = qp106;
+  }else if ((f == 53) && (m == 80)) {
+    var f1 = qp107;
+  }else if ((f == 54) && (m == 80)) {
+    var f1 = qp108;
+  }else if ((f == 55) && (m == 80)) {
+    var f1 = qp109;
+  }else if ((f == 56) && (m == 80)) {
+    var f1 = qp110;
+  }else if ((f == 57) && (m == 80)) {
+    var f1 = qp111;
+  }else if ((f == 58) && (m == 80)) {
+    var f1 = qp112;
+  }else if ((f == 59) && (m == 80)) {
+    var f1 = qp113;
+  }else if ((f == 60) && (m == 80)) {
+    var f1 = qp114;
+  }else if ((f == 61) && (m == 80)) {
+    var f1 = qp115;
+  }else if ((f == 62) && (m == 80)) {
+    var f1 = qp116;
+  }else if ((f == 63) && (m == 80)) {
+    var f1 = qp117;
+  }else if ((f >= 64) && (f <= 80) && (m == 80)) {
+    var f1 = qp118;
+  }else {    
     var f1 = qp20;
   }
 
 
   var sum = parseFloat(a1) + parseFloat(b1) + parseFloat(c1) + parseFloat(d1) + parseFloat(e1) + parseFloat(f1);
   var raw = (sum/g);
-  var result = raw.toFixed(2)
-  document.getElementById("r").textContent = "Your GPA is:"+ result;
+  var result = raw.toFixed(2) //reduce resultant number upto 2 decimal points.
+  document.getElementById("r").textContent = "Your GPA is:  "+ result;
   return false;
 }
+
+console.log("All Good");
